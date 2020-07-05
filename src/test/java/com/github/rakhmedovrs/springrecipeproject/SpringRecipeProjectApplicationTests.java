81d56b657cf6afd1e86af3413e60b768d353230b
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-05 20:45:14 +0300

    merge

--- src/test/java/com/github/rakhmedovrs/springrecipeproject/SpringRecipeProjectApplicationTests.java
+++ src/test/java/com/github/rakhmedovrs/springrecipeproject/SpringRecipeProjectApplicationTests.java
@@ -1,12 +1,11 @@
 package com.github.rakhmedovrs.springrecipeproject;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringRecipeProjectApplicationTests
 {
-
 	@Test
 	void contextLoads()
 	{
